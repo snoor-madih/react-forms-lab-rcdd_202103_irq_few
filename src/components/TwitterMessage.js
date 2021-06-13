2e8097e2065c231044b0f7d963edd2cf2d3527e8
snoor_madih <snoor.madih@gmail.com>
2021-06-13 19:18:52 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,24 +1,3 @@
-// import React from "react";
-
-// class TwitterMessage extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <strong>Your message:</strong>
-//         <input type="text" name="message" id="message" />
-//       </div>
-//     );
-//   }
-// }
-
-// export default TwitterMessage;
-
 import React from "react";
 
 class TwitterMessage extends React.Component {
